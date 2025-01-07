c05a9c906eb689df8b792b4667350af115e3690c
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-01-08 04:31:27 +0530

    gg maps with props revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
 import React from 'react'
 import Jokesnew from './Components/Jokesnew'
+import JokesData from './jokesData'
 
 
 const App = () => {
+
+  const ele = JokesData.map(function(item){
+    return (
+      <Jokesnew 
+        setup = {item.Question} 
+        punchline = {item.punchline}  
+        upVotes = {item.Up}
+          />
+    )
+  })
   return (
     <div>
-      <Jokesnew 
-        setup = "I got my daughter a fridge for her birthday."
-        punchline = "I can't wait to see her face light up when she opens it"
-        upVotes = "5"
-        downVotes = "6"
-       />
-      <Jokesnew 
-        setup = "What's the best thing about Switzerland?"
-        punchline = "I don't know, but the flag is a big plus!"
-        upVotes = "5"
-        downVotes = "6"
-       />
-      <Jokesnew 
-        setup = "Jokes HEHHEHE"
-        punchline = "Bhow Bhow!!"
-        upVotes = "5"
-        downVotes = "6"
-       />
-      <Jokesnew 
-        setup = "Woof Woof"
-        punchline = "Bhow Bhow!!"
-        upVotes = "5"
-        downVotes = {6}
-       />
-      <Jokesnew 
-        setup = "Maggi Noodles"
-        punchline = "feeling hundy"
-        upVotes = {5}
-       />
+      {ele}
     </div>
   )
 }
 
 export default App
+
+
+// <Jokesnew 
+// setup = "I got my daughter a fridge for her birthday."
+// punchline = "I can't wait to see her face light up when she opens it"
+// upVotes = "5"
+// downVotes = "6"
+// />
+// <Jokesnew 
+// setup = "What's the best thing about Switzerland?"
+// punchline = "I don't know, but the flag is a big plus!"
+// upVotes = "5"
+// downVotes = "6"
+// />
+// <Jokesnew 
+// setup = "Jokes HEHHEHE"
+// punchline = "Bhow Bhow!!"
+// upVotes = "5"
+// downVotes = "6"
+// />
+// <Jokesnew 
+// setup = "Woof Woof"
+// punchline = "Bhow Bhow!!"
+// upVotes = "5"
+// downVotes = {6}
+// />
+// <Jokesnew 
+// setup = "Maggi Noodles"
+// punchline = "feeling hundy"
+// upVotes = {5}
+// />
